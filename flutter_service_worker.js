899d15f5b16c5ceff8adc8de85984a3b94e97707
IlhamAbdul07 <IlhamAbdul07@users.noreply.github.com>
2025-08-18 07:03:27 +0000

    deploy: 3784497c44600be7ffb1432414f2e664e1729aab

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2531214abdd4cee2f8c14f7e234223c9",
+const RESOURCES = {"flutter_bootstrap.js": "df03130bb7797c72de345a545d43d675",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
