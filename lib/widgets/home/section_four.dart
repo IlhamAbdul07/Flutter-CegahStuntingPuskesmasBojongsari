91613aa8727bd4e6034994e970839f3f42344188
Ilham Abdul Hakim <70672465+IlhamAbdul07@users.noreply.github.com>
2025-08-29 15:13:26 +0700

    done versi #6, solve section 4

--- lib/widgets/home/section_four.dart
+++ lib/widgets/home/section_four.dart
@@ -9,44 +9,35 @@ class SectionFour extends StatelessWidget {
     return SizedBox(
       width: double.infinity,
       height: 450.0,
-      child: Expanded(
-        child: Row(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  top: 75,
-                  left: 75,
-                  right: 75,
-                  bottom: 75,
-                ),
-                child: Text(
-                  InfoText.description[2],
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontSize:
-                        MediaQuery.of(context).size.width *
-                        0.018, // responsif (1.8% lebar layar)
-                    color: Colors.black87,
-                    height: 1.5, // biar enak dibaca (line spacing)
-                  ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(75),
+              child: Text(
+                InfoText.description[2],
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width * 0.018,
+                  color: Colors.black87,
+                  height: 1.5,
                 ),
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Image.asset(
-                  'assets/home2.png',
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  height: MediaQuery.of(context).size.width * 0.5,
-                ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Image.asset(
+                'assets/home2.png',
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.width * 0.5,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
